63fd7906b52a720c74032f8f4f6fe4eb7f6efb9d
jpogah <jpogah@gmail.com>
2020-03-28 00:28:51 -0500

    bundle react front end with springboot backend

---
+++ src/main/java/com/unazi/graduateprograms/AuthenticationBean.java
@@ -0,0 +1,13 @@
+//package com.unazi.graduateprograms;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import lombok.RequiredArgsConstructor;
+//
+//@Data
+//@AllArgsConstructor
+//public class AuthenticationBean {
+//
+//
+//    private String message;
+//}
--- src/main/java/com/unazi/graduateprograms/GraduateprogramsApplication.java
+++ src/main/java/com/unazi/graduateprograms/GraduateprogramsApplication.java
@@ -2,6 +2,9 @@ package com.unazi.graduateprograms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GraduateprogramsApplication {
@@ -11,7 +14,15 @@ public class GraduateprogramsApplication {
         SpringApplication.run(GraduateprogramsApplication.class, args);
     }
 
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 
 
 }
