82b942302f0f2f940aa5a3a6edcdf55c21db5411
jpogah <jpogah@gmail.com>
2020-03-27 20:05:09 -0500

    revert previous change

---
+++ src/main/java/com/unazi/graduateprograms/controller/HomeController.java
@@ -0,0 +1,15 @@
+//package com.unazi.graduateprograms.controller;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//public class HomeController {
+//
+//
+//    @RequestMapping(value = "/")
+//    public String index() {
+//        return "index";
+//
+//    }
+//
+//
+//}
--- src/main/java/com/unazi/graduateprograms/model/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unazi.graduateprograms.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
